dbfac6976fcc3568342c218e8e6a233eec648c1b
87penginnouta <49667189+87penginnouta@users.noreply.github.com>
2021-05-04 07:17:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 AOS.init();
+
+/*-----loading-----*/
+
+$(window).on("load", function () {
+    $('html').css('overflow-y', 'auto');
+    $(".loading_wrapper").fadeOut("slow");
+    $("#bg").css('visibility', 'visible');
+});
